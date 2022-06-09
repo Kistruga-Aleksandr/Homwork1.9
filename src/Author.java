23f546a3de7ebe9c8537a3ec197f769ceb130014
Александр <a@kistruga.ru>
2022-06-09 18:27:01 +0400

    ДЗ № 9 Обьекты и классы Испарил

--- src/Author.java
+++ src/Author.java
@@ -1,14 +1,16 @@
 public class Author {
     private final String name;
     private final String surname;
-    public Author(String name, String surname){
+
+    public Author(String name, String surname) {
         this.name = name;
-        this.surname =surname;
+        this.surname = surname;
     }
 
     public String getName() {
         return name;
     }
+
     public String getSurname() {
         return surname;
     }
--- src/Book.java
+++ src/Book.java
@@ -1,13 +1,23 @@
 public class Book {
+    Author author;
     private int year;
+    private final String title;
 
-    public Book(int year, String author){
+    public Book(int year, String title, Author author) {
         this.year = year;
+        this.title = title;
+        this.author = author;
     }
-    public int getYear(){
+
+    public int getYear() {
         return year;
     }
-    public void setYear(int year){
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setYear(int year) {
         this.year = year;
     }
 
