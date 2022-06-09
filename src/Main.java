b3aeb50df211524415ea3dd188d52b63a599f6ab
Александр <a@kistruga.ru>
2022-06-09 18:30:05 +0400

    ДЗ № 9 Обьекты и классы Испарил поменя на UTF - 8

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@
 
 public class Main {
     public static void main(String[] args) {
-        Author author = new Author("Фёдор", "Достоевский");
-        Book book = new Book(1868, "Идиот", author);
-        Author author1 = new Author("Лев", "Толстой");
-        Book book1 = new Book(1868, "Война и мир", author1);
+        Author author = new Author("Р¤С‘РґРѕСЂ", "Р”РѕСЃС‚РѕРµРІСЃРєРёР№");
+        Book book = new Book(1868, "РРґРёРѕС‚", author);
+        Author author1 = new Author("Р›РµРІ", "РўРѕР»СЃС‚РѕР№");
+        Book book1 = new Book(1868, "Р’РѕР№РЅР° Рё РјРёСЂ", author1);
         book1.setYear(1873);
     }
 }
